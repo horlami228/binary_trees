8b87b4c3ed7c5b27b7a242f0926dad3003c26ce2
Olamilekan <akintolaolamilekan51@gmail.com>
2023-08-30 23:32:08 +0100

    binary_fix

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -7,9 +7,13 @@
 */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	if (parent == NULL)
+	{
+		return (NULL);
+	}
 	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
-	if (new_node == NULL || parent == NULL)
+	if (new_node == NULL)
 	{
 		return (NULL);
 	}
@@ -31,6 +35,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		*/
 		new_node->left = parent->left;
 		parent->left->parent = new_node;
+		parent->left = new_node;
 	}
 
 	return (new_node);
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -8,9 +8,11 @@
 */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
+	if (parent == NULL)
+		return (NULL);
 	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
-	if (new_node == NULL || parent == NULL)
+	if (new_node == NULL)
 	{
 		return (NULL);
 	}
@@ -32,6 +34,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		*/
 		new_node->right = parent->right;
 		parent->right->parent = new_node;
+		parent->right = new_node;
 	}
 
 	return (new_node);
