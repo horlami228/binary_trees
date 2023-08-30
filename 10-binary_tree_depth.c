088db0b2eb051f9db53085b719a850547510a502
Olamilekan <akintolaolamilekan51@gmail.com>
2023-08-30 23:53:08 +0100

    depth

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -6,12 +6,12 @@
 */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
+	size_t depth = 0;
+
 	if (tree == NULL)
 	{
 		return (0);
 	}
-
-	size_t depth = 0;
 
 	if (tree->parent != NULL)
 		depth += 1 + binary_tree_depth(tree->parent);
